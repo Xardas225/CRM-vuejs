9b29fa46e5be66e65850593b17f6ddc74a4af419
Alexander Derevkov <78501617+Xardas225@users.noreply.github.com>
2023-03-20 18:30:07 +0300

    add vuelidate, use validations in login, register page

--- src/main.js
+++ src/main.js
@@ -3,11 +3,12 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import "materialize-css/dist/js/materialize";
 import moment from 'moment';
+import useVuelidate from "@vuelidate/core";
+import "materialize-css/dist/js/materialize";
 
 let app = createApp(App);
 
 app.config.globalProperties.$moment=moment;
 
-app.use(store).use(router).mount("#app");
+app.use(useVuelidate).use(store).use(router).mount("#app");
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,25 +1,90 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
-        <input id="email" type="text" />
+        <input
+          id="email"
+          type="text"
+          v-model="email_register"
+          :class="{
+            invalid:
+              (v$.email_register.$dirty && !v$.email_register.required) ||
+              (v$.email_register.$dirty && v$.email_register.email),
+          }"
+        />
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small
+          class="helper-text invalid"
+          v-if="v$.email_register.$dirty && !v$.email_register.required"
+        >
+          Введите Email
+        </small>
+        <small
+          class="helper-text invalid"
+          v-else-if="v$.email_register.$dirty && v$.email_register.email"
+        >
+          Email некорректный
+        </small>
       </div>
       <div class="input-field">
-        <input id="password" type="password" class="validate" />
+        <input
+          id="password"
+          type="password"
+          v-model="password_register"
+          :class="{
+            invalid:
+              (v$.password_register.$dirty && !v$.password_register.required) ||
+              (v$.password_register.$dirty && v$.password_register.minLength),
+          }"
+        />
         <label for="password">Пароль</label>
-        <small class="helper-text invalid">Password</small>
+        <small
+          class="helper-text invalid"
+          v-if="v$.password_register.$dirty && !v$.password_register.required"
+        >
+          Введите пароль
+        </small>
+        <small
+          class="helper-text invalid"
+          v-else-if="v$.password_register.$dirty && v$.password_register.minLength"
+        >
+          Пароль должен быть не меньше
+          {{ v$.password_register.minLength.$params.min }} символов
+        </small>
       </div>
       <div class="input-field">
-        <input id="name" type="text" class="validate" />
+        <input
+          id="name"
+          type="text"
+          v-model="name"
+          :class="{
+            invalid:
+              (v$.name.$dirty && !v$.name.required) ||
+              (v$.name.$dirty && v$.name.minLength),
+          }"
+        />
         <label for="name">Имя</label>
-        <small class="helper-text invalid">Name</small>
+        <small
+          class="helper-text invalid"
+          v-if="v$.name.$dirty && !v$.name.required"
+        >
+          Введите bмя
+        </small>
+        <small
+          class="helper-text invalid"
+          v-else-if="v$.name.$dirty && v$.name.minLength"
+        >
+          Имя должно быть не меньше
+          {{ v$.name.minLength.$params.min }} символов
+        </small>
       </div>
       <p>
         <label>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            :class="{ invalid: v$.checkbox.$dirty && !v$.checkbox.sameAs }"
+          />
           <span>С правилами согласен</span>
         </label>
       </p>
@@ -34,14 +99,43 @@
 
       <p class="center">
         Уже есть аккаунт?
-        <a href="/">Войти!</a>
+        <router-link to="/login">Войти!</router-link>
       </p>
     </div>
   </form>
 </template>
 
 <script>
+import useVuelidate from "@vuelidate/core";
+import { required, minLength, email, sameAs } from "@vuelidate/validators";
+
 export default {
-    name: 'v-register'
-}
+  name: "v-register",
+  setup() {
+    return { v$: useVuelidate() }
+  },
+  data() {
+    return {
+      email: "",
+      password: "",
+      name: "",
+      checkbox: false,
+    };
+  },
+  validations: () => ({
+      email_register: { required, email },
+      password_register: { required, minLength: minLength(6) },
+      name: { required, minLength: minLength(2) },
+      checkbox: { sameAs: sameAs(() => true) },
+  }),
+  methods: {
+    submitHandler() {
+      if (this.v$.$error) {
+        this.v$.touch();
+        return;
+      }
+      this.$router.push("/");
+    },
+  },
+};
 </script>
